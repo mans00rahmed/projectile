ddd5465a9ab33afdf41d189bbc8328d4b555153c
mans00rahmed <mansoorahmedy2k@gmail.com>
2022-04-07 03:05:23 +0500

    database & admin panel

--- base/models.py
+++ base/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return(self.name)
+
+class Room(models.Model):
+    host = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True) 
+
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    #participants
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return(self.name)
+
+
+class Message(models.Model):
+    #this is to make a relation of 1-Many in code,b y adding foreign key...
+    user = models.ForeignKey(User,on_delete=models.CASCADE) # this cas casde will delete all message once room is deleted.
+    room = models.ForeignKey(Room,on_delete=models.CASCADE) # this cas casde will delete all message once room is deleted.
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return(self.body[0:50])
+
+
+    
--- base/views.py
+++ base/views.py
@@ -1,33 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseForbidden
-
+from .models import Room
 # Create your views here.
 
-rooms = [
+# rooms = [
 
-        {'id' : 1, 'name': 'Lets learn Python'},
-        {'id' : 2, 'name': 'Lets learn Php'},
-        {'id' : 3, 'name': 'Lets learn html'},
-        {'id' : 4, 'name': 'Lets learn css'},
-        {'id' : 5, 'name': 'Lets learn js'},
-        {'id' : 6, 'name': 'Lets learn ruby'},
-        {'id' : 7, 'name': 'Lets learn R'},
-        {'id' : 8, 'name': 'Lets learn c'},
-        {'id' : 9, 'name': 'Lets learn c#'},
+#         {'id' : 1, 'name': 'Lets learn Python'},
+#         {'id' : 2, 'name': 'Lets learn Php'},
+#         {'id' : 3, 'name': 'Lets learn html'},
+#         {'id' : 4, 'name': 'Lets learn css'},
+#         {'id' : 5, 'name': 'Lets learn js'},
+#         {'id' : 6, 'name': 'Lets learn ruby'},
+#         {'id' : 7, 'name': 'Lets learn R'},
+#         {'id' : 8, 'name': 'Lets learn c'},
+#         {'id' : 9, 'name': 'Lets learn c#'},
+#         {'id' : 10, 'name': 'Lets learn C++'},
         
-]
+        
+# ]
 
 
 # Functions for rendering
 
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+        rooms = Room.objects.all()
+        return render(request, 'home.html', {'rooms': rooms})
 
 
 def room(request,pk):
-    for i in rooms:
-        if i['id'] == int(pk):
-            room=i
+    room = Room.objects.get(id=pk)
     context={'room': room}
     return render(request, 'room.html',context)
