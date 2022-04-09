3d93271bda9882b9dd864d12c8fb00c70117238c
mans00rahmed <mansoorahmedy2k@gmail.com>
2022-04-10 01:22:54 +0500

    create form CRUD

--- base/models.py
+++ base/models.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -31,6 +32,3 @@ class Message(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return(self.body[0:50])
-
-
-    
--- base/views.py
+++ base/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render , redirect
 from django.http import HttpResponse, HttpResponseForbidden
+
+from .forms import RoomForm
 from .models import Room
 # Create your views here.
 
@@ -32,3 +35,14 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context={'room': room}
     return render(request, 'room.html',context)
+
+def create_room(request):
+        form = RoomForm()
+        if request.method == 'POST':
+                form = RoomForm(request.POST)
+                if form.is_valid():
+                        form.save()
+                        return redirect('home')
+
+        context={'form': form}
+        return(render(request, 'base/room_form.html', context))
