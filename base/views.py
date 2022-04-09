240d993f56dbc828632bce721e9b90aacc4fab56
mans00rahmed <mansoorahmedy2k@gmail.com>
2022-04-10 03:01:39 +0500

    delete from CRUD

--- base/views.py
+++ base/views.py
@@ -59,3 +59,12 @@ def update_room(request,pk):
                         return redirect('home')
         context={'form': form}
         return render(request, 'base/room_form.html',context)
+
+def delete_room(request,pk):
+        room = Room.objects.get(id=pk)
+        if request.method=='POST':
+                room.delete()
+                return redirect('home')
+
+        return render(request, 'base/delete.html',{'obj':room})
+
